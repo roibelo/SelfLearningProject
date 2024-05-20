8fcf723bd1f93052f33b239b07fd955407834eca
Roi Belo <roibelo@gmail.com>
2024-05-21 00:40:49 +0300

    commit

--- frontend_testing.py
+++ frontend_testing.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 
 try:
     driver = webdriver.Chrome(service=Service('C:/Users/Admin/Downloads/chromedriver.exe'))
-    driver.get('http://localhost:5001/users/get_user_data/2')
+    driver.get('http://localhost:5001/users/get_user_data/1')
     element = driver.find_element(By.ID, value='user')
     print(element.text)
 except Exception as ex:
